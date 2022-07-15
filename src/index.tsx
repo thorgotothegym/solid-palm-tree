54f985ddb93420876e469ea57053b95dfbc3398f
Aleix Vidal <al3ix.vidal@gmail.com>
2022-07-15 12:34:35 +0200

    implement queryClient to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import { Layout } from "./petrol/ui/pages/Layout";
+
 import reportWebVitals from "./reportWebVitals";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Layout />
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
